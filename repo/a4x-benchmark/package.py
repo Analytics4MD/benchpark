d6ac5112e364b87eab7d57d69db36707b8084031
Ian Lumsden <lumsden.ian@gmail.com>
2024-11-06 09:39:43 -0500

    Adds a version for the "many_to_one" branch for testing

--- repo/a4x-benchmark/package.py
+++ repo/a4x-benchmark/package.py
@@ -8,6 +8,7 @@ class A4xBenchmark(CachedCMakePackage):
     version("main", branch="main")
     version("0.1.0", tag="v0.1.0")
 
+    version("many_to_one", branch="many_to_one")
     version("summary_print", branch="summary_print")
     version("expanded_perf", branch="expanded_perf")
 
